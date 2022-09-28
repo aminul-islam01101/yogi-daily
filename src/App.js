829a3e9380436abd169677582a55d0c1cc4ef100
aminulislam01101 <aminul.islam01101@gmail.com>
2022-09-28 20:53:05 +0600

    folder setup with CRA

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="flex items-center justify-between min-h-screen w-full">
+            <div>hello world</div>
+        </div>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+const daisyPlugin = require('daisyui');
+
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            width: {
+                95: '95%',
+                90: '90%',
+                85: '85%',
+                80: '80%',
+                70: '70%',
+                60: '60%',
+                40: '40%',
+            },
+            maxWidth: {
+                95: '95%',
+                90: '90%',
+                85: '85%',
+                80: '80%',
+                70: '70%',
+                60: '60%',
+                40: '40%',
+            },
+        },
+    },
+    plugins: [daisyPlugin],
+    daisyui: {
+        styled: true,
+        themes: true,
+        base: false,
+        utils: true,
+        logs: true,
+        rtl: false,
+        prefix: '',
+        darkTheme: 'dark',
+    },
+};
