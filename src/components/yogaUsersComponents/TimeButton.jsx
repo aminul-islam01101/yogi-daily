4ba53553c60dc9d5659411617823f024577dacc4
aminulislam01101 <aminul.islam01101@gmail.com>
2022-09-29 22:33:12 +0600

    time button hover state added

--- src/components/yogaUsersComponents/TimeButton.jsx
+++ src/components/yogaUsersComponents/TimeButton.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const TimeButton = ({ time, breakTimeHandler }) => (
     <div className="avatar">
-        <div className="rounded-full ring ring-offset-green-700 ring-offset-2">
+        <div className="rounded-full ring ring-offset-green-600 ring-offset-2 ">
             <button
                 type="button"
-                className="text-black bg-yellow-300 rounded-full p-2"
+                className="text-black bg-yellow-300 hover:bg-green-600 hover:text-white rounded-full p-2 transition ease-in-out delay-150"
                 onClick={() => breakTimeHandler(time)}
             >
                 {time}s
